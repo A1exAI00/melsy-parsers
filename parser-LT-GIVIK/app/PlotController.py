2f5ab9b09c5ead16abf107c19e2c33820d335580
Alex Akinin <lksndrakinin@gmail.com>
2025-10-16 14:45:54 +0300

    Add some signals

--- parser-LT-GIVIK/app/PlotController.py
+++ parser-LT-GIVIK/app/PlotController.py
@@ -6,6 +6,8 @@ class PlotController(QObject):
     draggable_visibility_toggled = Signal(int)
     draggable_changed_position = Signal(int)
     touch_plot = Signal()
+    touch_legend = Signal()
+    update_ticks = Signal(tuple)
 
     def __init__(self):
         super().__init__()
