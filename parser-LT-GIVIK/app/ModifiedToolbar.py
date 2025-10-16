ac35dcc0df3116c615241c2a2251ad7a8bf4d3e7
Alex Akinin <lksndrakinin@gmail.com>
2025-10-16 14:44:51 +0300

    Fix toolbar not saving bug

--- parser-LT-GIVIK/app/ModifiedToolbar.py
+++ parser-LT-GIVIK/app/ModifiedToolbar.py
@@ -1,19 +1,19 @@
 from matplotlib.backends.backend_qtagg import NavigationToolbar2QT as NavigationToolbar
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
 
 
 class ModifiedToolbar(NavigationToolbar):
-    def __init__(self, canvas: FigureCanvas, parent):
+    def __init__(self, canvas: FigureCanvas, fig: Figure, parent):
         self.canvas = canvas
+        self.fig = fig
         super().__init__(self.canvas, parent)
         return
 
     def save_figure(self, *args):
         """Modify this function to always save figure with the same resolution"""
-        self.canvas.fig.tight_layout()
-        prev_width, prev_height = self.canvas.fig.get_size_inches()
-        self.canvas.fig.set_size_inches(10, 8)
+        prev_width, prev_height = self.fig.get_size_inches()
+        self.fig.set_size_inches(10, 8)
         super().save_figure(*args)
-        self.canvas.fig.set_size_inches(prev_width, prev_height)
-        self.canvas.fig.tight_layout()
+        self.fig.set_size_inches(prev_width, prev_height)
         return
